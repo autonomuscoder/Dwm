6264f422248518146e7195be82595fe55527ebe6
autonomuscoder <mahiabirgit@tutanota.com>
2023-07-18 20:34:15 +0600

    adding sxhkd

---
+++ autostart.sh
@@ -0,0 +1,14 @@
+# Autostart applications
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+slstatus &
+nitrogen --restore; sleep 1; 
+picom &
+numlockx on &
+nm-applet &
+volumeicon &
+dunst &
+blueman-applet &
+cbatticon &
+
+# sxhkd
+sxhkd -c ~/.dwm/sxhkdrc &
