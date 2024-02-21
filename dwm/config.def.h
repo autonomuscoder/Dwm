b673766934fc1d6b1e12c15ba367d87732414107
autonomuscoder <mahiabirgit@tutanota.com>
2024-02-21 16:06:34 +0600

    tokyo night colors

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -1,14 +1,14 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx = 0; /* border pixel of windows */
+static const unsigned int borderpx = 1; /* border pixel of windows */
 static const unsigned int snap = 32;    /* snap pixel */
-static const unsigned int gappih = 10;  /* horiz inner gap between windows */
-static const unsigned int gappiv = 10;  /* vert inner gap between windows */
+static const unsigned int gappih = 20;  /* horiz inner gap between windows */
+static const unsigned int gappiv = 20;  /* vert inner gap between windows */
 static const unsigned int gappoh =
-    10; /* horiz outer gap between windows and screen edge */
+    20; /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov =
-    10; /* vert outer gap between windows and screen edge */
+    20; /* vert outer gap between windows and screen edge */
 static const int smartgaps =
     0; /* 1 means no outer gap when there is only one window */
 
@@ -26,26 +26,26 @@ static const int showbar = 1;     /* 0 means no bar */
 static const int topbar = 1;      /* 0 means bottom bar */
 static const char *fonts[] = {"JetBrainsMono Nerd Font:size=10"};
 static const char dmenufont[] = "JetBrainsMono Nerd Font:size=10";
-static const char col_gray1[] = "#141414";
+static const char col_gray1[] = "#000000";
 static const char col_gray2[] = "#444444";
-static const char col_gray3[] = "#c0caf5";
-static const char col_gray4[] = "#c0caf5";
+static const char col_gray3[] = "#e0af68";
+static const char col_gray4[] = "#e0af68";
 // static const char col_cyan[]        = "#24283b";
-static const char col_cyan[] = "#1b1c1c";
-static const char col_red[] = "#2e383c";
+static const char col_cyan[] = "#1a1b26";
+static const char col_red[] = "#7dcfff";
 static const char *colors[][3] = {
     /*               fg         bg         border   */
-    [SchemeNorm] = {col_gray3, col_gray1, col_gray2},
+    [SchemeNorm] = {col_gray3, col_gray1, col_red},
     [SchemeSel] = {col_gray4, col_cyan, col_red},
 };
 
 typedef struct {
-  const char *name;
-  const void *cmd;
+    const char *name;
+    const void *cmd;
 } Sp;
 const char *spcmd1[] = {"st", "-n", "spterm", "-g", "120x34", NULL};
 const char *spcmd2[] = {"st",     "-n", "spfm",   "-g",
-                        "144x41", "-e", "ranger", NULL};
+    "144x41", "-e", "ranger", NULL};
 const char *spcmd3[] = {"keepassxc", NULL};
 static Sp scratchpads[] = {
     /* name          cmd  */
@@ -55,8 +55,9 @@ static Sp scratchpads[] = {
 };
 
 /* tagging */
-static const char *tags[] = {"一", "二", "三", "四", "五",
-                             "六", "七", "八", "九"};
+static const char *tags[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+// static const char *tags[] = {"一", "二", "三", "四", "五",
+//     "六", "七", "八", "九"};
 // static const char *tags[] = { "", "", "", "", ""};
 
 static const Rule rules[] = {
@@ -92,16 +93,16 @@ static const Layout layouts[] = {
 /* key definitions */
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY, TAG)                                                      \
-  {MODKEY, KEY, view, {.ui = 1 << TAG}},                                       \
-      {MODKEY | ControlMask, KEY, toggleview, {.ui = 1 << TAG}},               \
-      {MODKEY | ShiftMask, KEY, tag, {.ui = 1 << TAG}},                        \
-      {MODKEY | ControlMask | ShiftMask, KEY, toggletag, {.ui = 1 << TAG}},
+{MODKEY, KEY, view, {.ui = 1 << TAG}},                                       \
+    {MODKEY | ControlMask, KEY, toggleview, {.ui = 1 << TAG}},               \
+    {MODKEY | ShiftMask, KEY, tag, {.ui = 1 << TAG}},                        \
+    {MODKEY | ControlMask | ShiftMask, KEY, toggletag, {.ui = 1 << TAG}},
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd)                                                             \
-  {                                                                            \
+{                                                                            \
     .v = (const char *[]) { "/bin/sh", "-c", cmd, NULL }                       \
-  }
+}
 
 /* commands */
 static char dmenumon[2] =
@@ -113,14 +114,14 @@ static const char *termcmd[] = {"kitty", NULL};
 
 static const Key keys[] = {
     /* modifier                     key        function        argument */
-    {MODKEY, XK_Return, spawn, SHCMD("kitty")},
-    {MODKEY, XK_b, spawn, SHCMD("firefox")},
-    {MODKEY, XK_e, spawn, SHCMD("thunar")},
+    {MODKEY, XK_Return, spawn, SHCMD("alacritty")},
+    {MODKEY, XK_b, spawn, SHCMD("brave-browser")},
+    {MODKEY, XK_e, spawn, SHCMD("nemo")},
     {MODKEY, XK_r, spawn, SHCMD("redshift -O 5000")},
     {MODKEY | ShiftMask, XK_r, spawn, SHCMD("redshift -x")},
     {MODKEY, XK_s, spawn, SHCMD("flameshot gui")},
     {MODKEY | ShiftMask, XK_s, spawn, SHCMD("/home/xero/bin/./fzf-nvim")},
-    {MODKEY, XK_x, spawn, SHCMD("archlinux-logout")},
+    // {MODKEY, XK_x, spawn, SHCMD("archlinux-logout")},
     {MODKEY | ControlMask, XK_Return, spawn, {.v = spcmd1}},
     {MODKEY | ShiftMask, XK_Return, spawn, SHCMD("rofi -show drun")},
     {MODKEY, XK_j, focusstack, {.i = +1}},
@@ -162,8 +163,8 @@ static const Key keys[] = {
     {MODKEY, XK_y, togglescratch, {.ui = 0}},
     {MODKEY, XK_u, togglescratch, {.ui = 1}},
     TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
-        TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
-            TAGKEYS(XK_9, 8){MODKEY | ShiftMask, XK_q, quit, {0}},
+    TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
+    TAGKEYS(XK_9, 8){MODKEY | ShiftMask, XK_q, quit, {0}},
 };
 
 /* button definitions */
